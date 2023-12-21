ab2ef951811a915c225ccb9ba2302d3454b9f3e1
lefrianDinata <lefriandinata@MacBook-Air-Lefrian.local>
2023-12-21 21:21:32 +0700

    tugas selesai

---
+++ variable.js
@@ -0,0 +1,7 @@
+//tapilkan data menggunakan ``
+let namadepan = "lefrian";
+let namabelakang = "dinata";
+// halo nama saya variable variable
+let data = "halo nama saya"+" "+ namadepan+" "+ namabelakang +" "+"saya sedang belajar javascript";
+//outputnya = halo nama aku lefrian dinata saya sedang belajar javascript
+console.log(data);
